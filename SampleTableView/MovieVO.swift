c8515a0005c83c496401a7d145def381c9a01eb2
parkjunghun <go9018@naver.com>
2019-01-16 12:54:23 +0900

    Initial Commit

---
+++ SampleTableView/MovieVO.swift
@@ -0,0 +1,19 @@
+//
+//  MovieVO.swift
+//  SampleTableView
+//
+//  Created by 박정훈 on 16/01/2019.
+//  Copyright © 2019 swift. All rights reserved.
+//
+
+import Foundation
+
+//데이터소스 만들기 vo -> value object
+class MovieVO{
+    var thumbnail: String?
+    var title: String?
+    var description: String?
+    var detail: String?
+    var opendate: String?
+    var rating: Double?
+}
