ecbfc1420987113528e219edc66d5d92e297e52d
parkjunghun <go9018@naver.com>
2019-01-24 15:02:34 +0900

    [2019.01.24] 커스텀 클래스를 사용해서 Tableview 만들기

--- SampleTableView/ListViewController.swift
+++ SampleTableView/ListViewController.swift
@@ -41,17 +41,27 @@ class ListViewController: UITableViewController{
         //주어진 행에 맞는 데이터 소스를 읽어온다.
         let row = self.list[indexPath.row]
         //테이블 셀 객체를 직접 생성하는 대신 큐로부터 가져옴
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell") as! MovieCell
         
-        let title = cell.viewWithTag(101) as? UILabel
-        let desc = cell.viewWithTag(102) as? UILabel
-        let opendate = cell.viewWithTag(103) as? UILabel
-        let rating = cell.viewWithTag(104) as? UILabel
+        //데이터 소스에 저장된 값을 각 아울렛 변수에 할당
+        cell.title?.text = row.title
+        cell.desc?.text = row.description
+        cell.opendate?.text = row.opendate
+        cell.rating?.text = "\(row.rating!)"
         
-        title?.text = row.title
-        desc?.text = row.description
-        opendate?.text = row.opendate
-        rating?.text = "\(row.rating!)"
+        
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
+//
+//        let title = cell.viewWithTag(101) as? UILabel
+//        let desc = cell.viewWithTag(102) as? UILabel
+//        let opendate = cell.viewWithTag(103) as? UILabel
+//        let rating = cell.viewWithTag(104) as? UILabel
+//
+//        title?.text = row.title
+//        desc?.text = row.description
+//        opendate?.text = row.opendate
+//        rating?.text = "\(row.rating!)"
+//
 //        cell.textLabel?.text = row.title
 //        cell.detailTextLabel?.text = row.description
         return cell
---
+++ SampleTableView/MovieCell.swift
@@ -0,0 +1,19 @@
+//
+//  MovieCell.swift
+//  SampleTableView
+//
+//  Created by 박정훈 on 16/01/2019.
+//  Copyright © 2019 swift. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MovieCell: UITableViewCell{
+    
+    @IBOutlet weak var rating: UILabel!
+    @IBOutlet weak var opendate: UILabel!
+    @IBOutlet weak var desc: UILabel!
+    @IBOutlet weak var title: UILabel!
+    
+}
